5e7cd72b5287f58e6cef7f3e5184a0caeb3cc07b
Ashie Sharma <ashie.sharma1209@gmail.com>
2026-07-12 02:13:15 +0530

    Added loading state for visualization startup

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,47 @@
+import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
+import { EffectComposer, Bloom } from "@react-three/postprocessing";
+import { Html } from "@react-three/drei";
 import Scene from "./components/Scene";
 import CameraRig from "./components/CameraRig";
-import { EffectComposer, Bloom } from "@react-three/postprocessing";
+
+function LoadingScreen() {
+  return (
+    <Html center>
+      <div
+        style={{
+          color: "#8fd3ff",
+          fontFamily: "Arial, sans-serif",
+          letterSpacing: "0.18em",
+          fontSize: "14px",
+          textTransform: "uppercase",
+          whiteSpace: "nowrap",
+        }}
+      >
+        Initializing SynapseFlow...
+      </div>
+    </Html>
+  );
+}
+
 export default function App() {
   return (
     <Canvas camera={{ position: [0, 3.2, 8], fov: 55 }}>
       <color attach="background" args={["#02040a"]} />
       <fog attach="fog" args={["#02040a", 5, 16]} />
 
-      <Scene />
-      <CameraRig />
+      <Suspense fallback={<LoadingScreen />}>
+        <Scene />
+        <CameraRig />
 
-      <EffectComposer>
-        <Bloom
-          intensity={1.2}
-          luminanceThreshold={0.18}
-          luminanceSmoothing={0.95}
-        />
-      </EffectComposer>
+        <EffectComposer>
+          <Bloom
+            intensity={1.2}
+            luminanceThreshold={0.18}
+            luminanceSmoothing={0.95}
+          />
+        </EffectComposer>
+      </Suspense>
     </Canvas>
   );
 }
